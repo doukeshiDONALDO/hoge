d2b768fa870e066bb44ce50ecec3b306e7d93dcd
doukeshiDONALDO <b1401730@planet.kanazawa-it.ac.jp>
2019-12-02 16:57:45 +0900

    pickle module test

---
+++ reinforced/pickleread.py
@@ -0,0 +1,6 @@
+import pickle
+
+with open("auau.test","rb") as f:
+    a = pickle.load(f)
+    
+print(a)
---
+++ reinforced/picklewrite.py
@@ -0,0 +1,5 @@
+import pickle
+
+a = [12,334,5,54,34,33]
+with open("auau.test","wb") as f:
+    pickle.dump(a,f)
