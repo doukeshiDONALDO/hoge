ebb1d6415dcb5a5f94e2969c0269e6ab3d595551
doukeshiDONALDO <b1401730@planet.kanazawa-it.ac.jp>
2019-12-05 17:41:36 +0900

    q_learning.py epsilon.ver

--- reinforced/q_learning.py
+++ reinforced/q_learning.py
@@ -4,25 +4,40 @@
 import numpy as np
 from time import sleep
 import sqlite3
+import sys
+from sys import argv
+import pickle
 
-def get_status():
-    state = np.random.uniform(0, 10)
-    return state
+class QQ():
+    def __init__(self):
+        self.qtable = np.zeros((8,8))
+        self.state = None
+        self.action = None
+
+"""    def state(self):
+        return self.state
+
+    def set_state(self,value):
+        self.state = value
+    
+    def action(self):
+        return self.action
+
+    def set_action(self,value):
+        self.action = value
+   """     
+
 
 def get_action(_q_table):
     epsilon = 0.002
     if np.random.uniform(0, 1) > epsilon:
-        state = get_status()
-        _action = np.argmax(_q_table[state])
+        _action = np.argmax(_q_table[qq.action])
     else:
-        _action = np.random.choice([0, 1, 2])
+        _action = np.random.choice([0,1,2,3,4,5,6,7])
     return _action
 
-def get_reward():
 
 
-
-    return 
 
 def update_q_table(_q_table, _action, _reward):
 
@@ -30,38 +45,43 @@ def update_q_table(_q_table, _action, _reward):
     gamma = 0.99# 時間割引き率
 
     # 行動後の状態で得られる最大行動価値 Q(s',a')
-    next_position, next_velocity = get_status()
-    next_max_q_value = max(_q_table[next_position][next_velocity])
+    next_max_q_value = max(_q_table[qq.action])
 
     # 行動前の状態の行動価値 Q(s,a)
-    state = get_status()
-    q_value = _q_table[state][_action]
+    q_value = _q_table[qq.state][qq.action]
 
     # 行動価値関数の更新
     # q(s,a) = q(s,a) + α(R(s,a) + γmaxq(s',a') - q(s,a))
-    _q_table[position][velocity][_action] = q_value + alpha * (_reward + gamma * next_max_q_value - q_value)
+    _q_table[qq.state][qq.action] = q_value + alpha * (_reward + gamma * next_max_q_value - q_value)
 
-    return _q_table
 
 if __name__ == '__main__':
 
+    if sys.argv[1] == 'init':
+        qq = QQ()
+        qq.state = 0
+        qq.action = get_action(qq.qtable)
+        with open("qtable","wb") as f:
+            pickle.dump(qq,f)
+        sys.exit()
+    else:
+        with open("qtable","rb") as f:
+            qq = pickle.load(f)
 
-    # Qテーブルの初期化
-    q_table = np.zeros((8, 8))
-
-    rewards = []
-
+        reward = int(sys.argv[1])
 
         
-    # ε-グリーディ法で行動を選択
-    action = get_action(q_table)
+        # ε-グリーディ法で行動を選択
+        action = get_action(qq.qtable)
+        print('action: {}'.format(action))
 
-    # 車を動かし、観測結果・報酬・ゲーム終了FLG・詳細情報を取得
-    reward = get_reward()
+        # Qテーブルの更新
+        update_q_table(qq.qtable, action, reward)
 
+        print(qq.qtable)
+        qq.state = qq.action
+        qq.action = action
 
-    # Qテーブルの更新
-    q_table = update_q_table(q_table, action, observation, next_observation, reward, episode)
-
-
-
+    with open("qtable","wb") as f:
+        pickle.dump(qq,f)
+    
